cd49fc2bf432d6f16226ea7c7c6fc89f91992066
Sean Narvasa <sean@snarvasa.com>
2016-02-06 21:29:35 -0800

    get logic working

--- CalculatorMidTerm/CalculatorMidTerm/CalculatorViewController.swift
+++ CalculatorMidTerm/CalculatorMidTerm/CalculatorViewController.swift
@@ -10,88 +10,156 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    var numA:Int = 0
-    var numB:Int = 0
+    var numA:Int?
+    var numB:Int?
+    var operand:String?
+    var number:String = ""
     
     @IBOutlet weak var outputLabel: UILabel!
     
-
+    // Numerical buttons
     @IBAction func zeroButton(sender: AnyObject) {
-        let numA = 0
-        outputLabel?.text = "0"
-        print("\(numA)")
-        
+        number = number + "0"
+        outputLabel.text = "\(number)"
     }
     @IBAction func oneButton(sender: AnyObject) {
-        let numB = 1
-        outputLabel?.text = "1"
+        number = number + "1"
+        outputLabel.text = "\(number)"
     }
     @IBAction func twoButton(sender: AnyObject) {
-        _ = 2
-        outputLabel?.text = "2"
+        number = number + "2"
+        outputLabel.text = "\(number)"
     }
     @IBAction func threeButton(sender: AnyObject) {
-        _ = 3
-        outputLabel?.text = "3"
+        number = number + "3"
+        outputLabel.text = "\(number)"
     }
     @IBAction func fourButton(sender: AnyObject) {
-        _ = 4
-        outputLabel?.text = "4"
+        number = number + "4"
+        outputLabel.text = "\(number)"
     }
     @IBAction func fiveButton(sender: AnyObject) {
-        _ = 5
-        outputLabel?.text = "5"
+        number = number + "5"
+        outputLabel.text = "\(number)"
     }
     @IBAction func sixButton(sender: AnyObject) {
-        _ = 6
-        outputLabel?.text = "6"
+        number = number + "6"
+        outputLabel.text = "\(number)"
     }
     @IBAction func sevenButton(sender: AnyObject) {
-        _ = 7
-        outputLabel?.text = "7"
+        number = number + "7"
+        outputLabel.text = "\(number)"
     }
     @IBAction func eightButton(sender: AnyObject) {
-        _ = 8
-        outputLabel?.text = "8"
+        number = number + "8"
+        outputLabel.text = "\(number)"
     }
     @IBAction func nineButton(sender: AnyObject) {
-        _ = 9
-        outputLabel?.text = "9"
+        number = number + "9"
+        outputLabel.text = "\(number)"
     }
+    
+    
+    // Operand buttons
     @IBAction func clearButton(sender: AnyObject) {
-        outputLabel?.text = "0"
-        print("clear")
+        numA = nil
+        numB = nil
+        operand = nil
+        number = ""
+        outputLabel.text = "0"
     }
+    
     @IBAction func divideButton(sender: AnyObject) {
+        if operand == nil {
+            numA = Int(number)
+            operand = "divide"
+            number = ""
+        } else if operand == "equals" {
+            operand = "divide"
+        } else {
+            calculator()
+            operand = "divide"
+        }
     }
+    
     @IBAction func multiplyButton(sender: AnyObject) {
+        if operand == nil {
+            numA = Int(number)
+            operand = "multiply"
+            number = ""
+        } else if operand == "equals" {
+            operand = "multiply"
+        } else {
+            calculator()
+            operand = "multiply"
+        }
     }
+    
     @IBAction func subtractButton(sender: AnyObject) {
+        if operand == nil {
+            numA = Int(number)
+            operand = "subtract"
+            number = ""
+        } else if operand == "equals" {
+            operand = "subtract"
+        } else {
+            calculator()
+            operand = "subtract"
+        }
     }
+    
     @IBAction func addButton(sender: AnyObject) {
-        
+        if operand == nil {
+            numA = Int(number)
+            operand = "add"
+            number = ""
+        } else if operand == "equals" {
+            operand = "add"
+        } else {
+            calculator()
+            operand = "add"
+        }
     }
+    
     @IBAction func equalsButton(sender: AnyObject) {
-        outputLabel?.text = "5"
+        calculator()
+        operand = "calculate"
     }
  
+   // Where the magic happens 
+    func calculator() {
+        if let input = Int(number) {
+            numB = input
+            var numberInMemory:Int = 0
+            
+            if operand == "add" {
+                numberInMemory = numA! + numB!
+                
+            } else if operand == "subtract" {
+                
+                numberInMemory = numA! - numB!
+                
+            }  else if operand == "divide" {
+                
+                numberInMemory = numA! / numB!
+                
+            }  else if operand == "multiply" {
+                
+                numberInMemory = numA! * numB!
+            }
+            
+            outputLabel.text = "\(numberInMemory)"
+            numA = numberInMemory
+            number = ""
+            }
+        
+        }
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
 
 
 
